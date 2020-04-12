1f454c953d3cec92642ff17bfb160b00d50e3b9e
Supasin S <supasinet@gmail.com>
2020-04-12 13:38:19 +0700

    relocate database/ and routes/

--- src/InventoryServiceProvider.php
+++ src/InventoryServiceProvider.php
@@ -8,11 +8,11 @@ class InventoryServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__ . '/routes/api.php');
+        $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
 
-        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
-        $this->loadFactoriesFrom(__DIR__ . '/database/factories');
+        $this->loadFactoriesFrom(__DIR__ . '/../database/factories');
     }
 
     public function register()
